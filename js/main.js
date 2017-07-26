250f474452bb55a46b04064eff73a2757295bda2
Lisa Gee <ZhuSuFeng@gmail.com>
2017-07-26 08:41:06 -0700

    Destructure in eventListener

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ document.getElementById('calcBtn').addEventListener('click', function () {
     var principal = document.getElementById("principal").value;
     var years = document.getElementById("years").value;
     var rate = document.getElementById("rate").value;
-    var monthlyPayment = calculateMonthlyPayment(principal, years, rate);
+    let {monthlyPayment, monthlyRate} = calculateMonthlyPayment(principal, years, rate);
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
 });
